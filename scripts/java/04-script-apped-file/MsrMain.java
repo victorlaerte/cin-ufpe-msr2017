f6a2753b83ef12ef3e30e3a40bcb7d64d2b30d77
victorlaerte <victorlaertedoliveira@gmail.com>
2016-11-24 00:04:07 -0300

    fixing

--- scripts/java/04-script-apped-file/MsrMain.java
+++ scripts/java/04-script-apped-file/MsrMain.java
@@ -6,21 +6,40 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class MsrMain {
+public class MsrMain4 {
 
 	private static String file1Path = "/Users/victoroliveira/Downloads/MSR/tr_status_PASSED-tr_tests_failed_TRUE-60947-mined-copy.csv";
 	private static String file2Path = "/Users/victoroliveira/Downloads/MSR/tr_status_FAILED-tr_tests_failed_TRUE-103350-mined.csv";
 
 	public static void main(String[] args) {
 
-		mergeFiles();
+		String path1 = file1Path;
+		String path2 = file2Path;
+
+		if (args != null) {
+
+			for (int i = 0; i < args.length; i++) {
+
+				String currentArg = args[i];
+
+				if (i == 0 && currentArg != null) {
+					path1 = currentArg;
+				}
+				if (i == 1 && currentArg != null) {
+					path2 = currentArg;
+				}
+			}
+
+		}
+
+		appendFiles(path1, path2);
 	}
 
-	private static void mergeFiles() {
+	private static void appendFiles(String path1, String path2) {
 
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(file2Path)))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(path2)))) {
 
-			try (FileOutputStream fos = new FileOutputStream(new File(file1Path), true)) {
+			try (FileOutputStream fos = new FileOutputStream(new File(path2), true)) {
 
 				try (OutputStreamWriter osw = new OutputStreamWriter(fos)) {
 
@@ -36,8 +55,6 @@ public class MsrMain {
 								bw.newLine();
 							}
 						}
-
-						System.out.println("Total of " + i + " lines");
 					}
 				}
 			}
